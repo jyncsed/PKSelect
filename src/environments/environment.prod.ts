4bdba441fcb77f509da12218e73ed205118e78cb
James <jamesbridenthal@gmail.com>
2019-12-04 23:32:50 -0500

    Fixed automatic versioning

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  version: '0.0.3'
+  production: true,
+  version: '1.0.5'
  };
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   version: 'DEV'
- };
+};
 
 /*
  * For easier debugging in development mode, you can import the following file
