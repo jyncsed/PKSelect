8b928857bb8336118f024114cf34eafcfc0d9dce
James <jamesbridenthal@gmail.com>
2020-06-06 12:22:35 -0400

    new build for 1.0.16

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  version: '1.0.15',
-  buildDate: 'Sat Jun 06 2020 11:48:08 GMT-0400 (Eastern Daylight Time)'
+  version: '1.0.16',
+  buildDate: 'Sat Jun 06 2020 12:21:40 GMT-0400 (Eastern Daylight Time)'
  };
 
 
